a6c7d14ed03c2dc6fbdd2dab03bc000b22acbb5d
hatemBT <hatemtayeb2@gmail.com>
2018-04-30 11:01:24 +0100

    i am working now in EX3

--- les exercices/ex2/ex1.c
+++ les exercices/ex2/ex1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(int argc, *char argv[])
+int main(int argc, char *argv[])
 {
 int distance =100;
 
 printf("you are %d miles away. \n",distance);
-return 127;
+return 0;
 }
